2aa7fe37b8a8567dd15e777b32e9c557b31c8ff0
Satish-9567 <150673586+Satish-9567@users.noreply.github.com>
2024-01-28 18:51:51 +0530

    Update Workspace.java

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -12,7 +12,7 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-        super(super(emailId, Integer.MAX_VALUE);
+        super(emailId, Integer.MAX_VALUE);
         calendar = new ArrayList<>();
     }
 
